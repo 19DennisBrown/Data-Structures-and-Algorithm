ce820f4e05c72d0ae6f11b38da233ad009ffaf00
19DennisBrown <densmoney12@gmail.com>
2024-04-23 14:22:53 +0300

    Selection sort 3.0

---
+++ Arrays/Insertion_sort.py
@@ -0,0 +1,12 @@
+
+
+"""
+How it works:😁
+
+😊Take the first value from the unsorted part of the array.
+👍Move the value into the correct place in the sorted part of the array.
+😒Go through the unsorted part of the array again as many times as there are values.
+😒End
+"""
+
+array = [64, 34, 25, 12, 22, 11, 90, 5]
