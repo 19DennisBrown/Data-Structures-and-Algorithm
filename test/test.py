1ccc29802b1f9a2351aaf08b049c501b76f20f59
19DennisBrown <densmoney12@gmail.com>
2024-05-13 09:36:44 +0300

    Insertion test

--- test/test.py
+++ test/test.py
@@ -1,17 +1,19 @@
 
 array = [64, 34, 25, 12, 22, 112, 90, 5]
 
-n = len(array)
+input = int(input("Enter a number : "))
+array.append(input)
 
+n = len(array)
 for i in range(1, n):
   insert_index = i
-  insert_value = array[i]
+  current_value = array[i]
 
   for j in range(i-1, -1, -1):
-    if array[j] > insert_value:
+    if array[j] > current_value:
       array[j + 1] = array[j]
       insert_index = j
     else:
       break
-  array[insert_index] = insert_value
-print("Sorted array : ", array)
+  array[insert_index] = current_value
+print("INsertion sorted array : ", array)
