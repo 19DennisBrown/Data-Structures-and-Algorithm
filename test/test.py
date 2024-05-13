9937a6cf2886716552244f7941838b97830427b8
19DennisBrown <densmoney12@gmail.com>
2024-05-13 09:09:10 +0300

    Insertion sort

--- test/test.py
+++ test/test.py
@@ -1,17 +1,15 @@
 
 array = [64, 34, 25, 12, 22, 11, 90, 5]
+size = len(array)
 
-n = len(array)
-
-for i in range(1, n):
+for i in range(1,size):
   insert_index = i
-  current_value = array[i]
-
+  current_val = array[i]
   for j in range(i-1, -1, -1):
-    if array[j] > current_value:
+    if array[j] > current_val:
       array[j+1] = array[j]
-      insert_index = j
+      current_val = j
     else:
       break
-  array[insert_index] = current_value
-print("Insertion sort : ", array)
+  array[insert_index] = current_val
+print("Insertion sorted array : ", array)
