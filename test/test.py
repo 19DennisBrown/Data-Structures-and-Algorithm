f285f323d537387e8ffdec5538b708a13dbc10ec
19DennisBrown <densmoney12@gmail.com>
2024-05-14 08:44:53 +0300

    Test

--- test/test.py
+++ test/test.py
@@ -1,14 +1,10 @@
 arr = [2,4,6,8,12,32,45,66,434,12]
 size = len(arr)
 
-for i in range(1, size):
-  target_index = i
-  current_value = arr[i]
-  for j in range(i-1, -1, -1):
-    if arr[j]>arr[i]:
-      arr[j+1]=arr[j]
-      target_index = j
-    else:
-      break
-  arr[target_index]= current_value
+for i in range(size-1):
+  min_index = i
+  for j in range(i+1, size):
+    if arr[j]<arr[min_index]:
+      min_index=j
+    arr[i],arr[min_index]=arr[min_index],arr[i]
 print(arr)
