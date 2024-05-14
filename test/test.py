0706f6eac76a7f74493d4467980c76e2c52bc510
19DennisBrown <densmoney12@gmail.com>
2024-05-14 09:02:00 +0300

    'test'

--- test/test.py
+++ test/test.py
@@ -1,2 +1,12 @@
 arr = [2,4,6,8,12,32,45,66,434,12]
 
+n = len(arr)
+for i in range(n-1):
+  swapped=False
+  for j in range(n-i-1):
+    if arr[j]>arr[j+1]:
+      arr[j],arr[j+1] = arr[j+1],arr[j]
+      swapped = True
+    if not swapped:
+      break
+print(arr)
