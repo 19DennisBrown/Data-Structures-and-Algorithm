f166580794aa784ffc7414b93bf286930ab97be7
19DennisBrown <densmoney12@gmail.com>
2024-05-14 08:37:29 +0300

    Test

--- test/test.py
+++ test/test.py
@@ -1,19 +1,14 @@
+arr = [2,4,6,8,12,32,45,66,434,12]
+size = len(arr)
 
-array = [64, 34, 25, 12, 22, 112, 90, 5]
-
-input = int(input("Enter a number : "))
-array.append(input)
-
-n = len(array)
-for i in range(1, n):
-  insert_index = i
-  current_value = array[i]
-
+for i in range(1, size):
+  target_index = i
+  current_value = arr[i]
   for j in range(i-1, -1, -1):
-    if array[j] > current_value:
-      array[j + 1] = array[j]
-      insert_index = j
+    if arr[j]>arr[i]:
+      arr[j+1]=arr[j]
+      target_index = j
     else:
       break
-  array[insert_index] = current_value
-print("INsertion sorted array : ", array)
+  arr[target_index]= current_value
+print(arr)
