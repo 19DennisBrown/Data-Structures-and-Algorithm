c458f94f2baeac298ea4c2addb04e245bc9330ac
19DennisBrown <densmoney12@gmail.com>
2024-05-14 08:56:34 +0300

    'test'

--- test/test.py
+++ test/test.py
@@ -1,12 +1,2 @@
 arr = [2,4,6,8,12,32,45,66,434,12]
 
-input = int(input("Add new index value : "))
-arr.append(input)
-n = len(arr)
-for i in range(n-1):
-  index = i
-  for j in range(i+1, n):
-    if arr[j]<arr[index]:
-      index=j
-    arr[i],arr[index] = arr[index],arr[i]
-print(arr)
