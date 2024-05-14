49699f7f7bd873ed541e2dd00a3d991f5a6fa9c5
19DennisBrown <densmoney12@gmail.com>
2024-05-14 09:09:22 +0300

    'test'

--- test/test.py
+++ test/test.py
@@ -1,2 +1,12 @@
-arr = [2,4,6,8,12,32,45,66,434,12]
-
+arry = [2,4,6,8,12,32,45,66,434,12]
+arry.append(int(input("Enter value : ")))
+len = len(arry)
+for i in range(len-1):
+  swapped = False
+  for j in range(len-i-1):
+    if arry[j]>arry[j+1]:
+      arry[j],arry[j+1]=arry[j+1],arry[j]
+      swapped = True
+  if not swapped:
+    break
+print(arry)
